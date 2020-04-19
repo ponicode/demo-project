01f858a0e85f09c5c43f6e25e98bce24a7a885e5
BeFunes <benedetta.dalcanton@gmail.com>
2020-04-19 23:54:54 +0200

    A simple demo-project

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
-import styles from "./App.module.css";
+
 import Playground from "./components/Playground/Playground";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./common/styles/ponicode-bootstrap.css";
+
+import styles from "./App.module.css";
+import logo from "./images/logo.svg";
 
 function App() {
   return (
     <div className={styles.app}>
+      <img className={styles.logo} src={logo} height={32} alt="Ponicode" />
       <div className={styles.header}>Ponicode Test App</div>
       <div className={styles.container}>
         <Playground />
--- client/src/components/Users/Users.js
+++ client/src/components/Users/Users.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styles from "./Users.module.css";
 import api from "./../../utils/api";
 import { FaCheck, FaTimes } from "react-icons/fa";
+import { Table, Button, Form, Col } from "react-bootstrap";
 
 const acceptableAge = [];
 for (var i = 0; i <= 100; i++) {
@@ -93,60 +94,98 @@ function Users() {
     }
   }
 
+  function resetFilters() {
+    setMinAge("0");
+    setMaxAge("100");
+  }
+
+  function getEmailValidationIcon(user) {
+    return (
+      <span className={styles.emailValidIcon}>
+        {user.isEmailValid !== undefined &&
+          (!!user.isEmailValid ? (
+            <FaCheck color={"green"} />
+          ) : (
+            <FaTimes color={"red"} />
+          ))}
+      </span>
+    );
+  }
+
   return (
     <div className={styles.component}>
       <div className={styles.filter}>
-        <p>
-          min age:{" "}
-          <input
-            type={"number"}
-            value={minAge}
-            onChange={setMinAgeHandler}
-            onKeyDown={handleKeyDown}
-          />
-        </p>
-        <p>
-          max age:{" "}
-          <input
-            type={"number"}
-            value={maxAge}
-            onChange={setMaxAgeHandler}
-            onKeyDown={handleKeyDown}
-          />
-        </p>
-        <p className={styles.filterButton} onClick={() => getUsers()}>
-          FILTER USERS
-        </p>
+        <Form>
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Label>Minimum age</Form.Label>
+              <Form.Control
+                type={"number"}
+                value={minAge}
+                onChange={setMinAgeHandler}
+                onKeyDown={handleKeyDown}
+                placeholder="Enter minimum age"
+              />
+            </Form.Group>
+
+            <Form.Group as={Col}>
+              <Form.Label>Maximum age</Form.Label>
+              <Form.Control
+                type={"number"}
+                value={maxAge}
+                onChange={setMaxAgeHandler}
+                onKeyDown={handleKeyDown}
+                placeholder="Enter maximum age"
+              />
+            </Form.Group>
+          </Form.Row>
+          <Button variant="warning" size="sm" onClick={resetFilters}>
+            Reset
+          </Button>{" "}
+          <Button variant="primary" size="sm" onClick={getUsers}>
+            Filter
+          </Button>
+        </Form>
       </div>
       <div className={styles.usersSection}>
-        {users.map((x, i) => (
-          <div className={styles.userBlock} key={i}>
-            <div className={styles.userName}>{x.name}</div>
-            <div>
-              <span className={styles.header}>age: </span>
-              {x.age}
-            </div>
-
-            <div>
-              <span className={styles.header}>email: </span>
-              {x.email}
-              <span className={styles.emailValidIcon}>
-                {x.isEmailValid !== undefined &&
-                  (!!x.isEmailValid ? (
-                    <FaCheck color={"green"} />
-                  ) : (
-                    <FaTimes color={"red"} />
-                  ))}
-              </span>
-              <span
-                className={styles.checkEmailButton}
-                onClick={() => checkEmailIsValid(x.email)}
-              >
-                Check email validity
-              </span>
-            </div>
-          </div>
-        ))}
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Age</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>
+                  {user.address && `${user.address.road}, ${user.address.city}`}
+                </td>
+                <td>{user.age}</td>
+                <td>
+                  {user.email}
+                  {getEmailValidationIcon(user)}
+                </td>
+                <td>
+                  <Button
+                    variant="success"
+                    size="sm"
+                    onClick={() => checkEmailIsValid(user.email)}
+                    disabled={user.isEmailValid !== undefined}
+                  >
+                    Check email validity
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     </div>
   );
