6b91f73c0fa694d612aaa721c76b4a5177dcb05b
bilow <simon.guilliams@gmail.com>
2020-08-25 12:56:08 +0200

    Add demo function in JSX file

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,16 @@ import "./common/styles/ponicode-bootstrap.css";
 import styles from "./App.module.css";
 import logo from "./images/logo.svg";
 
+// Demo function, do right click and "Ponicode: Unit Test"
+export function isEmailValid(email) {
+  if (typeof email !== "string") {
+    return false;
+  }
+  const unicodePattern = /[^\x00-\x7F]/;
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return !unicodePattern.test(email) && re.test(email.toLowerCase());
+}
+
 function App() {
   return (
     <div className={styles.app}>
