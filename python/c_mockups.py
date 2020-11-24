6df1a92c5bd81a53e0be86d5eadac4a3c281de30
wbenbihi <walid@ponicode.com>
2020-11-24 17:22:42 +0100

    [ADD] Python mockups

---
+++ python/c_mockups.py
@@ -0,0 +1,31 @@
+import urllib
+
+URL = "https://api.exchangeratesapi.io/{date}?base={curr}&symbols={symbols}"
+
+# API request mockup
+# For the following arguments set ("2020-11-01", "EUR", "USD,JPY")
+# The response should be
+# {"rates":{"JPY":122.36,"USD":1.1698},"base":"EUR","date":"2020-10-30"}
+
+def get_request(url):
+    r = urllib.urlopen(url)
+    data = r.read()
+    r.close()
+    return d
+
+def get_currency_rate_from_urllib(date, base_currency, currencies):
+    rates = urllib.urlopen(
+        URL.format(
+            date=date, 
+            curr=base_currency, 
+            symbols=currencies
+        )
+    ).read()
+    return rates
+
+def get_currency_rate_from_custom(date, base_currency, currencies):
+    rates = get_request(url=URL.format(date=date, curr=base_currency, symbols=currencies))
+    return rates
+
+if __name__ == "__main__":
+    print(get_currency_rate("2020-11-01", "EUR", "USD,JPY"))
