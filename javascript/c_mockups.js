fc1dd567b12ed5d491ce9e8268b9639966281708
edmond <edmond@ponicode.dev>
2021-01-26 14:18:05 +0100

    [add] currency function

--- javascript/c_mockups.js
+++ javascript/c_mockups.js
@@ -4,7 +4,7 @@ let api = require('./z_utils')
 // API request mockup
 // For the following arguments set ("2020-11-01", "EUR", "USD,JPY")
 // The response should be
-// {"rates":{"JPY":122.36,"USD":1.1698},"base":"EUR","date":"2020-10-30"}
+// {"status":200, "data":{"rates":{"JPY":122.36,"USD":1.1698},"base":"EUR","date":"2020-10-30"}}
 async function getCurrencyRatesFromCustomJs(date, base_currency, currencies){
     const URL = "https://api.exchangeratesapi.io/"
     let url = `${URL}${date}?base=${base_currency}&symbols=${currencies}`
